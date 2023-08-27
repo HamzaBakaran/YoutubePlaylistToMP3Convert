30a93b558431a298519b08bf382be6b691d36bd5
HamzaBakaran <hamza.bakaran@stu.ibu.edu.ba>
2023-08-27 14:13:30 +0200

    Update

--- youtubeToMP3.py
+++ youtubeToMP3.py
@@ -10,9 +10,6 @@ yt = YouTube(input("Enter the URL of the video you want to download: \n>> "))
 # extract only audio
 video = yt.streams.filter(only_audio=True).first()
   
-
-# replace destination with the path where you want to save the downloaded file
-destination = "/home/hamza/Desktop/Python scripts"
   
 # download the file
 out_file = video.download(OUTPUT_PATH)
